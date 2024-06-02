64b7a555cf8b5f9f6158e7445127df85d0656e96
danielpmcconkey <danielpmcconkey@gmail.com>
2024-06-02 12:02:02 -0400

    KAN-26
    more unit tests

--- TestsBench/IntegrationTest1.cs
+++ TestsBench/IntegrationTest1.cs
@@ -9,6 +9,141 @@ namespace TestsBench.Tests
 {
     public class IntegrationTest1
     {
+        #region constants
+        const string _newBookTitle = "El gigante egoísta";
+        const string _newBookUrl = "https://www.youtube.com/watch?v=1iQh6IJwO9U";
+        const string _newBookLanguageCode = "ES";
+        const string _newBookText = @"El gigante egoísta
+De Óscar Wilde
+Versión en español por SpanishInput
+
+
+Primera parte: voy a construir una pared
+
+Cada tarde, después de la escuela
+Los niños iban a jugar al jardín del gigante
+Era un jardín grande y bonito
+Por todos lados había flores
+También había  árboles llenos  de flores
+A los pájaros les gustaba  descansar en los árboles y cantar
+A los niños, les gustaba oír a los pájaros
+Todos eran muy felices
+
+Un día, el gigante volvió 
+Había estado con un amigo por siete años
+Después de los siete años, volvió a su casa
+Llegó y vio a los niños jugando en jardín
+""¿Qué hacen aquí?"" dijo con voz fuerte
+Los niños se fueron corriendo
+""¡Mi jardín es mi jardín!"" dijo el gigante
+""¡Nadie más  puede venir a jugar aquí!""
+""¡Voy a construir una pared para que nadie venga aquí!""
+
+Hizo una pared grande alrededor del jardín
+Y puso un cartel que decía, ""no entrar""
+Era un gigante muy egoísta
+
+Segunda parte: corazón frío, jardín frío
+Los niños ahora no tenían dónde  jugar
+La calle estaba llena de polvo y piedras
+Así que  no les gustó jugar ahí 
+Un día, al salir de la escuela
+Fueron hasta la pared del jardín
+Se pusieron a recordar el jardín
+Dijeron, ""ahí  dentro éramos felices""
+Entonces vino la primavera
+Y por todo el país, había flores y pájaros
+Solo en el jardín del gigante, aún hacía frío
+Los pájaros no vinieron a cantar
+Porque no había niños
+Y los árboles no dieron flores
+Un día, salió un hermosa flor
+Pero cuando la flor vio el cartel
+Se puso triste por los niños, y volvió a suelo
+Solo el hielo estaba feliz. Dijo
+""La primavera no ha venido a este jardín,
+Asi que estaré aquí todo el año""
+El jardín se volvió frío y blanco.
+El hielo invitó al viento del norte, y vino
+El viento frió pasó todo el día en el jardín
+Y rompió las ventanas
+El gigante egoísta dijo
+""No entiendo por qué  no viene la primavera""
+""Quisiera que la primavera viniera  pronto""
+Sentado cerca de la ventana
+Vio  que su jardín estaba frío y blanco
+Pero la primavera nunca vino
+Así que en el jardín siempre hacía  frío
+El viento del norte y el hielo eran felices ahí 
+
+Tercera parte: el regreso de la primavera
+Una mañana, el gigante estaba en su cama
+Cuando escuchó una linda canción
+En realidad, solo era un pájaro
+Cantando afuera de  su ventana
+Pero hace mucho tiempo no había oído 
+A un pájaro cantar en su jardín
+Así que el canto del pájaro
+Le pareció la música más hermosa del mundo
+Entonces el hielo y el viento frío se fueron
+Y un agradable olor entró  por la ventana
+El gigante dijo:
+""Creo que por fin ha llegado la primavera""
+Así que salió de la cama y vio hacia afuera
+¿Qué vio?
+Vio una escena maravillosa
+Los niños habían entrado a través 
+De un pequeño hueco en la pared
+Cada niño estaba sentado en un árbol
+Los árboles estaban muy felices
+Así que cada árbol dio muchas flores
+Los pájaros estaban volando y cantando
+Era una escena hermosa
+Solo in un esquina aún había  hielo
+En este esquina había un niño pequeño
+Era (tan?) pequeño que no podía subir al árbol
+Y estaba llorando
+El árbol aún  estaba frío y con hielo
+El viento del norte aún estaba allí
+""¡Sube pequeño!"" dijo el árbol
+Pero el niño era demasiado pequeño
+
+Quarta parte: un cambio de actitud
+Cuando vio esto, el gigante se puso triste
+El gigante pensó:
+Ahora sé  por qué  no venía la primavera
+Voy a poner a ese niño sobre el árbol
+Y voy a destruir la pared
+También, voy a dejar que los niños
+Vengan a jugar en mi jardín para siempre
+Estaba muy triste por lo que había hecho
+Así que abrió la puerta  muy despacio
+Y salió al jardín
+Cuando los niños lo vieron
+Tuvieron miedo y se fueron corriendo
+El frío otra vez llegó al jardín
+Solo el niño pequeño no se fue
+Estaba llorando así que no vio al gigante
+El gigante lo tomó con cuidado en su mano
+Y lo puso sobre el árbol
+De inmediato, el árbol dio muchas flores
+Y los pájaros vinieron a cantar
+El pequeño niño dio un beso al  gigante
+Los otros niños se dieron  cuenta
+De que el gigante ya no era egoísta
+Los niños vinieron corriendo
+Yo con ellos vino la primavera
+El gigante les dijo los niños
+""Este jardín ahora es de ustedes, niños""
+Y empezó a destruir la pared
+Cuando la gente pasó por ahí
+Vieron al gigante jugando con los niños
+Era el jardín más hermoso del mundo
+
+Fin
+";
+        #endregion
+
         #region setup functions
         BookService? _bookService = null;
 
@@ -370,6 +505,58 @@ namespace TestsBench.Tests
                     bookUser.Book = null;
                     bookUser.PageUsers = new List<PageUser>();
                     context.BookUsers.Remove(bookUser);
+                }
+                user.LanguageUsers = new List<LanguageUser>();
+                context.Users.Remove(user);
+                context.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public async Task ParagraphTranslationWorks()
+        {
+            // arrange
+            var context = CreateContext();
+            var userService = CreateUserService();
+            var user = CreateNewTestUser(userService);
+            BookService BookService = new BookService();
+            // create the book
+            int bookId = await BookService.BookCreateAndSaveAsync(context, 
+                _newBookTitle, _newBookLanguageCode, _newBookUrl, _newBookText);
+            // add the book stats
+            BookService.BookStatsCreateAndSave(context, bookId);
+            // now create the book user for teh logged in user
+            int userId = (int)user.Id;
+            int bookUserId = await _bookService.BookUserCreateAndSaveAsync(context, bookId, userId);
+            
+            try
+            {
+                // act
+
+                if (BookService.IsDataInitRead == false)
+                {
+                    await BookService.InitDataRead(context, userService, bookId);
+                }
+                var pp = BookService.Paragraphs[0];
+                var translation = await BookService.ParagraphTranslate(context, pp);
+                var expectedTranslation = "The selfish giant";
+                var actualTranslation = pp.ParagraphTranslations[0].TranslationText;
+
+                // assert
+                Assert.Equal(expectedTranslation, actualTranslation);
+            }
+            finally
+            {
+                // clean-up
+                var book = context.Books.Where(x => x.Id == bookId).FirstOrDefault();
+                if (book != null)
+                {
+                    book.Language = null;
+                    book.Pages = null;
+                    book.BookStats = null;
+                    book.BookUserStats = null;
+                    book.BookUsers = null;
+                    context.Books.Remove(book);
                 }
                 user.LanguageUsers = new List<LanguageUser>();
                 context.Users.Remove(user);
