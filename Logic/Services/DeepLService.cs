3f1a17107c4b8e31677aca3bcd85939db88e8bd7
danielpmcconkey <danielpmcconkey@gmail.com>
2024-07-28 15:54:37 -0400

    fixing a bug in tag ui, adding a stub to the deepl call

--- Logic/Services/DeepLService.cs
+++ Logic/Services/DeepLService.cs
@@ -7,62 +7,34 @@ namespace Logic.Services
 {
     public static class DeepLService
     {
-        //private ILogger<IdiomaticaLogger> _logger;
-
-        //public DeepLService(ILogger<IdiomaticaLogger> logger)
-        //{
-        //    _logger = logger;
-        //}
+        
         public static async Task<string> TranslateAsync (
             string input, string sourceLanguageCode, string targetLanguageCode) 
         {
-            try
+            var authKey = Environment.GetEnvironmentVariable("DeepLApiKey");
+            if (string.IsNullOrEmpty(authKey))
             {
-                var authKey = Environment.GetEnvironmentVariable("DeepLApiKey");
-                if (string.IsNullOrEmpty(authKey))
-                {
-                    ErrorHandler.LogAndThrow();
-                    return string.Empty;
-                }
-                var translator = new Translator(authKey);
-
-                var translatedText = await translator.TranslateTextAsync(
-                      input,
-                      sourceLanguageCode,
-                      targetLanguageCode);
-                return translatedText.ToString();
+                ErrorHandler.LogAndThrow();
+                return string.Empty;
             }
-            catch (Exception ex)
-            {
-                string[] args = [
-                    $"input = {input}",
-                    $"sourceLanguageCode = {sourceLanguageCode}",
-                    $"targetLanguageCode = {targetLanguageCode}",
-                    ];
-                ErrorHandler.LogAndThrow(3010, args, ex);
-                throw; // you'll never get here
-            } 
+            var translator = new Translator(authKey);
+
+#if DEBUG
+            return "This is a stubbed result";
+#else
+
+            var translatedText = await translator.TranslateTextAsync(
+                    input,
+                    sourceLanguageCode,
+                    targetLanguageCode);
+            return translatedText.ToString();
+#endif
         }
 
         public static string Translate(
             string input, string sourceLanguageCode, string targetLanguageCode)
         {
-            try
-            {
-                var t = Task.Run(() => TranslateAsync(input, sourceLanguageCode, targetLanguageCode));
-                t.Wait();
-                return t.Result;
-            }
-            catch (Exception ex)
-            {
-                string[] args = [
-                    $"input = {input}",
-                    $"sourceLanguageCode = {sourceLanguageCode}",
-                    $"targetLanguageCode = {targetLanguageCode}",
-                    ];
-                ErrorHandler.LogAndThrow(3010, args, ex);
-                throw; // you'll never get here
-            }
+            return TranslateAsync(input, sourceLanguageCode, targetLanguageCode).Result;
         }
 
     }
