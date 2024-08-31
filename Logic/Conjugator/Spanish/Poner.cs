b2dea6402dc9f16003c877d1694e01b6ff27b816
danielpmcconkey <danielpmcconkey@gmail.com>
2024-08-31 13:46:20 -0400

    poder and poner

---
+++ Logic/Conjugator/Spanish/Poner.cs
@@ -0,0 +1,551 @@
+﻿using Logic.Telemetry;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Conjugator.Spanish
+{
+    public class Poner : _Er
+    {
+        public Poner(
+            IVerbTranslator targetTranslator, Verb sourceLanguageInfinitive, Verb targetLanguageInfinitive) :
+                base(targetTranslator, sourceLanguageInfinitive, targetLanguageInfinitive)
+        {
+
+        }
+
+        internal override List<VerbConjugationPiece> GetCorePiece2()
+        {
+            // pus
+            if (string.IsNullOrEmpty(_sourceLanguageInfinitive.Core2))
+            {
+                ErrorHandler.LogAndThrow();
+                return [];
+            }
+            string seek = "us";
+            return ReplaceRightOfCore(_sourceLanguageInfinitive.Core2, seek);
+        }
+
+
+        #region Present
+
+        public override List<VerbConjugationPiece> GetStemPiecesPresentYo()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "o"
+            }];
+        }
+        
+
+        #endregion
+
+        #region Preterite
+        public override List<VerbConjugationPiece> GetRootPiecesPreteriteYo()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesPreteriteYo()
+        {
+            return [new VerbConjugationPiece() {
+                Ordinal = 200, Type = AvailableVerbConjugationPieceType.IRREGULAR, Piece = "e"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesPreteriteTu()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesPreteriteEl()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesPreteriteEl()
+        {
+            return [new VerbConjugationPiece() {
+                Ordinal = 200, Type = AvailableVerbConjugationPieceType.IRREGULAR, Piece = "o"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesPreteriteNosotros()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesPreteriteVosotros()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesPreteriteEllos()
+        {
+            return GetCorePiece2();
+        }
+
+        #endregion
+
+        #region Conditional
+
+        public override List<VerbConjugationPiece> GetStemPiecesConditionalYo()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "ía"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesConditionalTu()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "ías"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesConditionalEl()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "ía"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesConditionalNosotros()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "íamos"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesConditionalVosotros()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "íais"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesConditionalEllos()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "ían"
+            }];
+        }
+        #endregion
+
+        #region Future
+
+        public override List<VerbConjugationPiece> GetStemPiecesFutureYo()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "é"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesFutureTu()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "ás"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesFutureEl()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "á"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesFutureNosotros()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "emos"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesFutureVosotros()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "éis"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesFutureEllos()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "dr"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "án"
+            }];
+        }
+
+        #endregion
+
+
+
+        #region SubjunctivePresent
+
+        public override List<VerbConjugationPiece> GetStemPiecesSubjunctivePresentYo()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "a"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesSubjunctivePresentTu()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "as"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesSubjunctivePresentEl()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "a"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesSubjunctivePresentNosotros()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "amos"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesSubjunctivePresentVosotros()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "áis"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesSubjunctivePresentEllos()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "an"
+            }];
+        }
+        #endregion
+
+        #region SubjunctiveImperfect
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveImperfectYo()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveImperfectTu()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveImperfectEl()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveImperfectNosotros()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveImperfectVosotros()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveImperfectEllos()
+        {
+            return GetCorePiece2();
+        }
+
+
+        #endregion
+
+        #region SubjunctiveFuture
+
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveFutureYo()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveFutureTu()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveFutureEl()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveFutureNosotros()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveFutureVosotros()
+        {
+            return GetCorePiece2();
+        }
+        public override List<VerbConjugationPiece> GetRootPiecesSubjunctiveFutureEllos()
+        {
+            return GetCorePiece2();
+        }
+        #endregion
+
+        #region AffirmativeImperative
+
+
+        public override List<VerbConjugationPiece> GetRootPiecesAffirmativeImperativeTu()
+        {
+            return GetCorePiece();
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesAffirmativeImperativeTu()
+        {
+            return [];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesAffirmativeImperativeUsted()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "a"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesAffirmativeImperativeNosotros()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "amos"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesAffirmativeImperativeUstedes()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "an"
+            }];
+        }
+
+
+        #endregion
+
+        #region NegativeImperative
+
+        public override List<VerbConjugationPiece> GetStemPiecesNegativeImperativeTu()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "as"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesNegativeImperativeUsted()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "a"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesNegativeImperativeNosotros()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "amos"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesNegativeImperativeVosotros()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "áis"
+            }];
+        }
+        public override List<VerbConjugationPiece> GetStemPiecesNegativeImperativeUstedes()
+        {
+            return [new VerbConjugationPiece()
+            {
+                Ordinal = 200,
+                Type = AvailableVerbConjugationPieceType.IRREGULAR,
+                Piece = "g"
+            },new VerbConjugationPiece()
+            {
+                Ordinal = 210,
+                Type = AvailableVerbConjugationPieceType.REGULAR,
+                Piece = "an"
+            }];
+        }
+        #endregion
+
+
+
+    }
+}
